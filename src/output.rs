ad51ad1b12eaae68d6b14bfedb8ee2a2982c7862
Brandon <brandon.wofford@unity3d.com>
2024-02-19 19:05:36 -0800

    much better tables

--- src/output.rs
+++ src/output.rs
@@ -1,5 +1,12 @@
 use crate::dns::Message as DNSMessage;
-use tabled::{settings::Panel, settings::Style, Table, Tabled};
+use tabled::{
+    settings::{
+        object::{Rows, Segment},
+        style::{HorizontalLine, Style},
+        Alignment, Modify, Padding, Width, Panel
+    },
+    Table,
+};
 
 // Assuming DNSMessage and other component structs are defined elsewhere
 pub struct Output {
@@ -15,49 +22,24 @@ impl Output {
         self.print_header();
         //self.print_questions();
         self.print_answers();
-        self.print_authority();
-        self.print_extra();
+        //self.print_authority();
+        //self.print_extra();
     }
 
     fn print_header(&self) {
         let header = &self.message.header;
         let table = Table::new(vec![header.clone()])
-            .with(Panel::header("Header"))
-            .with(Style::extended())
+            .with(Panel::vertical(0, "Header"))
+            .with(Style::modern().remove_horizontal())
             .to_string();
         println!("{}", table);
     }
-
-    //fn print_questions(&self) {
-    //    let questions = &self.message.question;
-    //    let table = Table::new(questions.clone()).to_string();
-    //    println!("Questions");
-    //    println!("{}", table);
-    //}
 
     fn print_answers(&self) {
         let answers = &self.message.answer;
         let table = Table::new(answers)
-            .with(Panel::header("Answers"))
-            .with(Style::extended())
-            .to_string();
-        println!("{}", table);
-    }
-
-    fn print_authority(&self) {
-        let authority = &self.message.authority;
-        let table = Table::new(authority)
-            .with(Panel::header("Authority"))
-            .with(Style::extended())
-            .to_string();
-        println!("{}", table);
-    }
-
-    fn print_extra(&self) {
-        let extra = &self.message.extra;
-        let table = Table::new(extra)
-            .with(Panel::header("Extra"))
-            .with(Style::extended())
+            .with(Panel::vertical(0, "Answers"))
+            .with(Style::modern().remove_horizontal())
             .to_string();
         println!("{}", table);
     }
